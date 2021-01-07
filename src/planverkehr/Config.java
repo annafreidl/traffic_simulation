875e03b744e1ee2a5f16d52733dc980b6fc054dc
Anna Monika Freidl <freidl@cip.ifi.lmu.de>
2021-01-07 18:14:17 +0100

    test

--- src/planverkehr/Config.java
+++ src/planverkehr/Config.java
@@ -4,7 +4,7 @@ import javafx.util.Duration;
 
 public class Config {
     public static final int scaleFactor = 20;
-    public static final int windowSize = 1000;
+    public static final int windowSize = 700;
     public static final String jsonFile = "big.json";
     public static final Duration tickFrequency = Duration.seconds(1);
 }
